0a85b84227257549d629e71bcc4fc0e9ffb59b39
pipi-shortstocking <jannie435@naver.com>
2024-11-05 01:25:42 +0900

    feat: 메인 작업 중

--- controller/IndexController.js
+++ controller/IndexController.js
@@ -1,7 +1,18 @@
+const { Play } = require('../models');
+
 // main 화면
 exports.index = async (req, res) => {
     try {
-        res.send({ result: true, message: "Hello World!" }); // nfc 태그 시, 진입 가능
+        let { play_id } = req.query;
+
+        let play = await Play.findOne({
+            where: { id: play_id }
+        })
+
+        // 포스터 전달 가공 필요
+
+        res.send({ play: play });
+        // res.send({ result: true, message: "Hello World!" }); // nfc 태그 시, 진입 가능
     } catch (err) {
         console.log(err);
     }
