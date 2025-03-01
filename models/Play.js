07d9a65fb1c1ea1094389fcbbfc960f8f81fbed3
pipi-shortstocking <jannie435@naver.com>
2025-03-01 17:33:23 +0900

    fix: 공연 정보 데이터(장소, 러닝 타임) 추가(#80)

--- models/Play.js
+++ models/Play.js
@@ -1,36 +1,46 @@
 const { DataTypes } = require('sequelize');
 
 const Play = (sequelize) => {
-    const model = sequelize.define(
-        'play', 
-        {
-            id: {
-                type: DataTypes.BIGINT,
-                allowNull: false,
-                primaryKey: true,
-                autoIncrement: true,
-                comment: '공연 pri 키',
-            },
-            title: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                comment: '공연 제목',
-            },
-            poster: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                comment: '공연 포스터',
-            },
-        },
-        {
-            tableName: 'play',
-            freezeTableName: true,
-            timestamps: false,
-            charset: 'utf8mb4',
-            collate: 'utf8mb4_general_ci',
-        }
-    );
-    return model;
+  const model = sequelize.define(
+    'play',
+    {
+      id: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        comment: '공연 pri 키',
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        comment: '공연 제목',
+      },
+      poster: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        comment: '공연 포스터',
+      },
+      locaton: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        comment: '공연 장소',
+      },
+      running_time: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        comment: '관람 시간',
+      },
+    },
+    {
+      tableName: 'play',
+      freezeTableName: true,
+      timestamps: false,
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+    }
+  );
+  return model;
 };
 
 module.exports = Play;
