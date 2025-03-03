a5db7ad25ce6b4965987c96c17e4de79e9decc1f
pipi-shortstocking <jannie435@naver.com>
2025-03-04 00:08:45 +0900

    fix: 평가 중복 처리 주석(#96)

--- controller/SurveyController.js
+++ controller/SurveyController.js
@@ -4,20 +4,20 @@ const { Survey } = require('../models');
 exports.questionOne = async (req, res) => {
   try {
     const { answer } = req.body;
-    const { id } = req.session.userInfo;
+    // const { id } = req.session.userInfo;
 
-    const user = await Survey.findOne({
-      where: {
-        user_id: id,
-        question: 1,
-      },
-    });
+    // const user = await Survey.findOne({
+    //   where: {
+    //     user_id: id,
+    //     question: 1,
+    //   },
+    // });
 
-    if (user) {
-      return res.status(400).send({
-        error: '이미 서비스 평가가 완료됨.',
-      });
-    }
+    // if (user) {
+    //   return res.status(400).send({
+    //     error: '이미 서비스 평가가 완료됨.',
+    //   });
+    // }
 
     if (!answer || answer < 1 || answer > 5) {
       return res.status(400).send({
@@ -28,7 +28,7 @@ exports.questionOne = async (req, res) => {
     await Survey.create({
       question: 1,
       answer: answer,
-      user_id: id,
+      // user_id: id,
     });
 
     res.send({ success: true });
@@ -42,20 +42,20 @@ exports.questionOne = async (req, res) => {
 exports.questionTwo = async (req, res) => {
   try {
     const { answer } = req.body;
-    const { id } = req.session.userInfo;
+    // const { id } = req.session.userInfo;
 
-    const user = await Survey.findOne({
-      where: {
-        user_id: id,
-        question: 2,
-      },
-    });
+    // const user = await Survey.findOne({
+    //   where: {
+    //     user_id: id,
+    //     question: 2,
+    //   },
+    // });
 
-    if (user) {
-      return res.status(400).send({
-        error: '이미 서비스 평가가 완료됨.',
-      });
-    }
+    // if (user) {
+    //   return res.status(400).send({
+    //     error: '이미 서비스 평가가 완료됨.',
+    //   });
+    // }
 
     if (!answer || answer < 0 || answer > 10) {
       return res.status(400).send({
@@ -66,7 +66,7 @@ exports.questionTwo = async (req, res) => {
     await Survey.create({
       question: 2,
       answer: answer,
-      user_id: id,
+      // user_id: id,
     });
 
     res.send({ success: true });
