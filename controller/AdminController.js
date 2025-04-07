94044351cca983a80a67bbebd0bf3146971d3467
pipi-shortstocking <jannie435@naver.com>
2025-04-07 23:31:05 +0900

    refactor: 어드민 로그인(#135)

--- controller/AdminController.js
+++ controller/AdminController.js
@@ -5,18 +5,18 @@ exports.enterAdmin = async (req, res) => {
   try {
     const { code } = req.query;
 
-    let adminCode = process.env.ADMIN_CODE;
-    let play_id = process.env.PLAY_ID; // 광운극회 단막극 공연 id
+    const adminCode = JSON.parse(process.env.ADMIN_CODE || '[]');
+    const admin = adminCode.find((admin) => admin.code === code);
 
-    if (code !== adminCode) {
+    if (!admin) {
       return res.status(400).send({
         error: '잘못된 인증코드',
       });
     }
 
     req.session.admin = {
-      code: adminCode,
-      play: play_id,
+      code: admin.code,
+      play: admin.play_id,
     };
 
     res.send({ success: true });
